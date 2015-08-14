8955f023a99e045cb95b0f9712260d3eae2ae61c
Tuukka Turto <tuukka.turto@oktaeder.net>
2015-08-14 21:01:55 +0300

    Add rudimentary main loop

--- brutopia/Program.fs
+++ brutopia/Program.fs
@@ -1,7 +1,50 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+
+exception UnknownSeason of Enum
+
+type Season =
+    | Spring = 0
+    | Autumn = 1
+
+type Model = {
+    year : int;
+    season : Season;
+    running : Boolean;
+}
+
+let processSpringSeason model =
+    {model with season = Season.Autumn }
+
+let processAutumnSeason model =
+    { model with year = model.year + 1
+                 season = Season.Spring }
+
+let advanceSeason model =
+    match model.season with
+        | Season.Autumn -> processAutumnSeason model
+        | Season.Spring -> processSpringSeason model
+        | _ -> raise (UnknownSeason model.season)
+
+let seasonText season =
+    match season with
+        | Season.Autumn -> "Autumn"
+        | Season.Spring -> "Spring"
+        | _ -> raise (UnknownSeason season)
+
+let outputStatus model =
+    printfn "Status for the year: %d" model.year
+    printfn "Current season: %s" (seasonText model.season)
+    model
+
+let rec mainLoop model =
+    let newModel = advanceSeason model |> outputStatus
+    Console.ReadKey() |> ignore
+    mainLoop newModel
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let model = { year = 1;
+                  season = Season.Spring;
+                  running = true }
+    mainLoop model    
     0 // return an integer exit code
